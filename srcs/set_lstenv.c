90cfe9280fd130bdf62cc6212e7c0f4a1f4ab390
Martin Degraeuwe <mdegraeu@z2r9p8.42lyon.fr>
2022-04-25 16:42:09 +0200

    split don't leaks (ft_free_dstr added)

--- srcs/set_lstenv.c
+++ srcs/set_lstenv.c
@@ -6,7 +6,7 @@
 /*   By: mdegraeu <mdegraeu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 11:24:42 by ltrinchi          #+#    #+#             */
-/*   Updated: 2022/04/25 16:06:57 by mdegraeu         ###   ########.fr       */
+/*   Updated: 2022/04/25 16:41:58 by mdegraeu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,7 @@ t_env *ft_set_lstenv(char **env)
 		new = malloc(sizeof(t_env));
 		new->varName = ft_strdup(split[0]);
 		new->value = ft_strdup(split[1]);
-		free(split[0]);
-		free(split[1]);
-		free(split);
+		ft_free_dstr(split);
 		new->next = NULL;
 		if (buff == NULL)
 			start = new;
